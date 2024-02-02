34f3b9ec3f6780d785e18290d3501ca33a6155b1
Joan <joanalemany26@gmail.com>
2024-02-02 13:56:30 +0100

    added middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,21 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '2d' })
+}
 
 module.exports = userController =  {
   signupUser: async (req, res) => {
     const {email, password} = req.body
     try {
-      await User.signup(email, password)
+      const user = await User.signup(email, password)
+      const { _id } = user;
   
-      res.status(200).json({email})
+      // create a token
+      const token = createToken(user._id)
+
+      res.status(200).json({ _id, email, token})
     } catch (error) {
       res.status(400).json({
         error: 'Bad Request',
@@ -19,9 +28,13 @@ module.exports = userController =  {
     const {email, password} = req.body
   
     try {
-      await User.login(email, password)
-  
-      res.status(200).json({email})
+      const user = await User.login(email, password)
+      const { _id } = user;
+
+      // create a token
+      const token = createToken(_id)
+
+      res.status(200).json({ _id, email, token})
     } catch (error) {
       res.status(400).json({
         error: 'Bad Request',
